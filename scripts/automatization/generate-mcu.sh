f4693975fa10961c843c48d301ad3c249f33242e
allpaul0 <paulallaireperso@gmail.com>
2025-11-10 09:59:40 +0100

    add script for automatic fpga build of py configs

---
+++ scripts/automatization/generate-mcu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source env.sh
+
+dir_py_config_MCUs=configs/mcu/python
+output_dir=experimentations/microarchitectures
+
+# Create output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+for config in "$dir_py_config_MCUs"/*.py; do
+    # Extract full path (already in $config)
+    config_path="$config"
+    # Extract filename without extension
+    config_name=$(basename "$config" .py)
+    # Remove the 'mcu_cfg_' prefix
+    config_name=${config_name#mcu_cfg_}
+
+    echo "mcu-gen for config: $config_name"
+
+    make mcu-gen CPU=cv32e20 BUS=onetoM MEM_BANKS=2 PYTHON_X_HEEP_CFG="$config_path"
+
+    echo "Vivado build for config: $config_name"
+
+    # synth,impl,bitstream
+    make vivado-fpga FPGA_BOARD=pynq-z2
+
+    echo "Finished config: $config_name"
+
+    mv build/ "$output_dir/$config_name"
+done
