cd3ad03b72425f78278a15d8786fc22fda7c7657
allpaul0 <paulallaireperso@gmail.com>
2025-11-23 14:39:31 +0100

    simulator generate-mcu_cv32e40px choose which CPU to build rather th build all

--- scripts/automatic-generation_simulator/generate-mcu_cv32e40px.sh
+++ scripts/automatic-generation_simulator/generate-mcu_cv32e40px.sh
@@ -1,30 +1,53 @@
 #!/bin/bash
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 {cv32e40px|cv32e40px_fpu|cv32e40px_corev_pulp_fpu|cv32e40px_corev_pulp}"
+    exit 1
+fi
+
+CONFIG="$1"
+
+case "$CONFIG" in
+    cv32e40px)
+        PARAMS=""
+        DEST="cv32e40px"
+        ;;
+
+    cv32e40px_fpu)
+        PARAMS='FUSESOC_PARAM="--X_EXT=1 --FPU=1"'
+        DEST="cv32e40px_fpu"
+        ;;
+
+    cv32e40px_corev_pulp_fpu)
+        PARAMS='FUSESOC_PARAM="--COREV_PULP=1 --X_EXT=1 --FPU=1"'
+        DEST="cv32e40px_corev_pulp_fpu"
+        ;;
+
+    cv32e40px_corev_pulp)
+        PARAMS='FUSESOC_PARAM="--COREV_PULP=1"'
+        DEST="cv32e40px_corev_pulp"
+        ;;
+
+    *)
+        echo "Invalid argument: $CONFIG"
+        echo "Valid options: cv32e40px cv32e40px_fpu cv32e40px_corev_pulp_fpu cv32e40px_corev_pulp"
+        exit 1
+        ;;
+esac
 
 source env.sh
 
-make mcu-gen X_HEEP_CFG=configs/mcu/hjson/cv32e40px.hjson 
+echo "=== Building config: $CONFIG ==="
+make mcu-gen X_HEEP_CFG=configs/mcu/hjson/cv32e40px.hjson
 
-make verilator-build 
+# Run verilator-build with optional params
+if [ -z "$PARAMS" ]; then
+    make verilator-build
+else
+    eval make verilator-build $PARAMS
+fi
 
-mv build/ experimentations/microarchitectures/simulators/cv32e40px
+mv build/ experimentations/microarchitectures/simulators/$DEST
 
-
-make mcu-gen X_HEEP_CFG=configs/mcu/hjson/cv32e40px.hjson 
-
-make verilator-build FUSESOC_PARAM="--X_EXT=1 --FPU=1"
-
-mv build/ experimentations/microarchitectures/simulators/cv32e40px_fpu
-
-
-make mcu-gen X_HEEP_CFG=configs/mcu/hjson/cv32e40px.hjson 
-
-make verilator-build FUSESOC_PARAM="--COREV_PULP=1 --X_EXT=1 --FPU=1"
-
-mv build/ experimentations/microarchitectures/simulators/cv32e40px_corev_pulp_fpu
-
-
-make mcu-gen X_HEEP_CFG=configs/mcu/hjson/cv32e40px.hjson 
-
-make verilator-build FUSESOC_PARAM="--COREV_PULP=1"
-
-mv build/ experimentations/microarchitectures/simulators/cv32e40px_corev_pulp
+echo "=== Done. Output moved to: experimentations/microarchitectures/simulators/$DEST ==="
