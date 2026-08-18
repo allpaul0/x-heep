45954511a5edd1ba5ec6f58c5cd326ce78a868e4
allpaul0 <paulallaireperso@gmail.com>
2026-08-18 17:30:38 +0200

    prod repo for dispatch

---
+++ sw/applications/tpg_modelization/tpg_inference_instrDispatch_instrTeams_instrTPG/codegen/externHeader.h
@@ -0,0 +1,39 @@
+#ifndef EXTERNHEADER
+#define EXTERNHEADER
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <float.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "../fixedptc/fixedptc.h"
+#include "../approximateComputingTools.h"
+
+#ifdef USE_INT
+  #warning "Using int as typeInf"
+  typedef int typeInf;  
+#elif defined(USE_FLOAT)
+  #warning "Using float as typeInf"
+  typedef float  typeInf;
+#elif defined(USE_FIXEDPT)
+  #warning "Using fixedpt as typeInf"
+  typedef fixedpt  typeInf;  // using fixedptc library
+#else  // default
+  #warning "Using float as typeInf"
+  typedef float    typeInf;
+#endif
+
+  inline typeInf convEnvToInf(double input){
+    #ifdef USE_FIXEDPT
+      return double_to_fixedpt(input);
+    #endif
+      return ((typeInf) input);
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // EXTERNHEADER
